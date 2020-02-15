3e43f94dcd45516e7560bc7f64af1331f107d219
Jose Maldonado <josemaldonadobusiness@gmail.com>
2020-02-15 13:52:11 -0500

    created and organized components

---
+++ client/src/components/Main.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+class Main extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+
+        }
+    }
+
+
+    render() {
+        return (
+            <div>
+
+            </div>
+        )
+    }
+}
+
+export default Main
