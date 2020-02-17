55aa2918bc593d8837767dc9d2c3a8201b6865e1
Errol Highberg <errol.highberg@gmail.com>
2020-02-17 14:26:16 -0500

    main page background and home row buttons complete

--- client/src/components/routes/Home.jsx
+++ client/src/components/routes/Home.jsx
@@ -1,40 +1,44 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import '../../css/Home.css'
 
 
 function Home() {
     return (
         <div className="home">
-            <div className="home-row">
-                <Link to="#">
-                    <img src={require('../../assets/airplane.svg')} />
-                    Flights
-                </Link>
-                <Link to="#">
-                    <img src={require('../../assets/hotel.svg')} />
-                    Hotels
-                </Link>
-                <Link to="#">
-                    <img src={require('../../assets/airplane.svg')} />
-                    <img src={require('../../assets/hotel.svg')} />
-                    Packages
-                </Link>
-                <Link to="#">
-                    <img src={require('../../assets/car.svg')} />
-                    Cars
-                </Link>
-                <Link to="#">
-                    <img src={require('../../assets/cruise.svg')} />
-                    Cruises
-                </Link>
-                <Link to="#">
-                    <img src={require('../../assets/camera.svg')} />
-                    Activities
-                </Link>
+            <div className='home-container'>
+                <div className="home-row">
+                    <Link to="#" className='home-row-button home-selected'>
+                        <img src={require('../../assets/airplane.svg')} />
+                        <p>Flights</p>
+                    </Link>
+                    <Link to="#" className='home-row-button'>
+                        <img src={require('../../assets/hotel.svg')} />
+                        <p>Hotels</p>
+                    </Link>
+                    <Link to="#" className='home-row-button'>
+                        <div>
+                            <img src={require('../../assets/airplane.svg')} />
+                            <img src={require('../../assets/hotel.svg')} />
+                        </div>
+                        <p>Packages</p>
+                    </Link>
+                    <Link to="#" className='home-row-button'>
+                        <img src={require('../../assets/car.svg')} />
+                        <p>Cars</p>
+                    </Link>
+                    <Link to="#" className='home-row-button'>
+                        <img src={require('../../assets/cruise.svg')} />
+                        <p>Cruises</p>
+                    </Link>
+                    <Link to="#" className='home-row-button'>
+                        <img src={require('../../assets/camera.svg')} />
+                        <p>Activities</p>
+                    </Link>
+                </div>
             </div>
         </div>
     )
 }
 
 export default Home
-
