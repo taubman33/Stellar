2d75943724c613804bf985cf5af77d86ac553cae
josemaldonado <josemaldonadobusiness@gmail.com>
2020-02-18 09:15:47 -0500

    Revert "Navbar"

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,26 @@
-import React from 'react'
-import { withRouter } from 'react-router-dom'
-import Main from './components/Main'
-
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
-    <div className="app">
-      <Main />
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
-  )
+  );
 }
 
-export default withRouter(App)
+export default App;
