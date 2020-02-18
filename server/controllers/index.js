1c3cbaeae9d7f22c430c808e8fc54beee1bd7e09
jeremy taubman <jeremytaubman@jeremys-MacBook-Air.local>
2020-02-18 10:51:44 -0500

    server is working

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -3,13 +3,7 @@ const { Flight, User, Itinerary } = require('../models')
 //get all users
 const getAllUsers = async (req, res) => {
     try {
-      const users = await User.findAll({
-        include: [
-          {
-            model: User
-          }
-        ]
-      });
+      const users = await User.findAll();
       return res.status(200).json({ users });
     } catch (error) {
       return res.status(500).send(error.message);
