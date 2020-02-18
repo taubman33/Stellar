56c6bec94a265fb5449d963f7d89deb93aa1821f
Errol Highberg <errol.highberg@gmail.com>
2020-02-18 11:52:05 -0500

    completed Loading page

--- client/src/components/Loading.jsx
+++ client/src/components/Loading.jsx
@@ -1,21 +1,24 @@
 import React from 'react'
+import '../css/Loading.css'
 
 const Loading = props => {
   return (
     <div className='loading'>
-      <h1>STELLAR</h1>
-      <img alt='loading icon' src={require('../assets/loading-circle.svg')} />
-      <div className='loading-text-line'>
-        <img alt='check-circle' src={require('../assets/check-circle.jpg')} />
-        <p>Earn and use Stellar Rewards</p>
-      </div>
-      <div className='loading-text-line'>
-        <img alt='dollar-circle' src={require('../assets/dollar-circle.svg')} />
-        <p>Get price guarantee</p>
-      </div>
-      <div className='loading-text-line'>
-        <img alt='phone-circle' src={require('../assets/phone-circle.svg')} />
-        <p>Book fast with our free mobile apps</p>
+      <h1 className='big-logo-loading'>STELLAR</h1>
+      <img alt='loading icon' src={require('../assets/loading-circle.svg')} className='loading-icon'/>
+      <div className='loading-lines'>
+        <p className='loading-text-line'>
+          <img alt='check-circle' src={require('../assets/check-circle.jpg')} />
+          Earn and use Stellar Rewards
+        </p>
+        <p className='loading-text-line'>
+          <img alt='dollar-circle' src={require('../assets/dollar-circle.svg')} />
+          Get price guarantee
+        </p>
+        <p className='loading-text-line'>
+          <img alt='phone-circle' src={require('../assets/phone-circle.svg')} />
+          Book fast with our free mobile apps
+        </p>
       </div>
     </div>
   )
