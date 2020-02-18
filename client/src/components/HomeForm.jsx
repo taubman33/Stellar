6e6377f469ecefe1ffa0cfc5cd8e945a2d7dd732
Errol Highberg <errol.highberg@gmail.com>
2020-02-18 15:22:41 -0500

    home calendar and passensger number dropdowns styled

--- client/src/components/HomeForm.jsx
+++ client/src/components/HomeForm.jsx
@@ -1,56 +1,60 @@
 import React from 'react'
 import { Formik, Field, Form } from 'formik'
-import DatePicker from 'react-date-picker'
+import '../css/Home.css'
 
 function HomeForm(props) {
   return (
-    <div>
+    <div className='home-form'>
       <Formik
         initialValues={{ adults: 1, children: 0, directFlights: false }}
         onSubmit={props.handleHomeSubmit}
       >
         <Form>
-          <label>
-            Departing
-           <input type="date" value={props.date.departure} onChange={props.handleDateChange} name="departure" />
-          </label>
-          <label>
-            Arrival
-            <input type="date" value={props.date.return} onChange={props.handleDateChange} name="return" />
-          </label>
-          <label>
-            <Field as="select" name="adults">
-              <option value="1">1</option>
-              <option value="2">2</option>
-              <option value="3">3</option>
-              <option value="4">4</option>
-              <option value="5">5</option>
-              <option value="6">6</option>
-            </Field>
-            Adults (18+)
-          </label>
-          <label>
-            <Field as="select" name="children">
-              <option value="0">0</option>
-              <option value="1">1</option>
-              <option value="2">2</option>
-              <option value="3">3</option>
-              <option value="4">4</option>
-              <option value="5">5</option>
-              <option value="6">6</option>
-            </Field>
-            Children (0-17)
-          </label>
+          <div className='home-calendar-numpassengers'>
+            <label className='home-label-above' id='departure-date'>
+              <p>Departing</p>
+              <img src={require('../assets/calendar.svg')} alt='calendar-icon' className='calendar-icon' />
+              <input type="date" value={props.date.departure} onChange={props.handleDateChange} name="departure" />
+            </label>
+            <label className='home-label-above' id='arrival-date'>
+              <p>Returning</p>
+              <img src={require('../assets/calendar.svg')} alt='calendar-icon' className='calendar-icon' />
+              <input type="date" value={props.date.return} onChange={props.handleDateChange} name="return" />
+            </label>
+            <label className='home-label-above'>
+              <p>Adults</p>
+              <Field as="select" name="adults">
+                <option value="1">1</option>
+                <option value="2">2</option>
+                <option value="3">3</option>
+                <option value="4">4</option>
+                <option value="5">5</option>
+                <option value="6">6</option>
+              </Field>
+            </label>
+            <label className='home-label-above'>
+              <p>Kids</p>
+              <Field as="select" name="children">
+                <option value="0">0</option>
+                <option value="1">1</option>
+                <option value="2">2</option>
+                <option value="3">3</option>
+                <option value="4">4</option>
+                <option value="5">5</option>
+                <option value="6">6</option>
+              </Field>
+            </label>
+          </div>
           <label>
             <Field type="checkbox" name="directFlights" />
-            Direct flights only
+            <p>Direct flights only</p>
           </label>
           <label>
             <Field type="checkbox" name="ecoFriendly" />
-            Eco-Friendly Option (Save $ by selecting this option)
+            <p>Eco-Friendly Option (Save $ by selecting this option)</p>
           </label>
           <label>
-          Preferred Class
+            <p>Preferred Class</p>
             <Field as="select" name="preferredClass">
               <option value="First class">First class</option>
               <option value="Business">Business</option>
--- client/src/components/routes/Home.jsx
+++ client/src/components/routes/Home.jsx
@@ -1,50 +1,47 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-<<<<<<< HEAD
 import '../../css/Home.css'
-=======
 import HomeForm from '../HomeForm'
->>>>>>> integration
 
 
 
 function Home(props) {
-    return (
-        <div className="home">
-            <div className='home-container'>
-                <div className="home-row">
-                    <Link to="#" className='home-row-button home-selected'>
-                        <img src={require('../../assets/airplane.svg')} />
-                        <p>Flights</p>
-                    </Link>
-                    <Link to="#" className='home-row-button'>
-                        <img src={require('../../assets/hotel.svg')} />
-                        <p>Hotels</p>
-                    </Link>
-                    <Link to="#" className='home-row-button'>
-                        <div>
-                            <img src={require('../../assets/airplane.svg')} />
-                            <img src={require('../../assets/hotel.svg')} />
-                        </div>
-                        <p>Packages</p>
-                    </Link>
-                    <Link to="#" className='home-row-button'>
-                        <img src={require('../../assets/car.svg')} />
-                        <p>Cars</p>
-                    </Link>
-                    <Link to="#" className='home-row-button'>
-                        <img src={require('../../assets/cruise.svg')} />
-                        <p>Cruises</p>
-                    </Link>
-                    <Link to="#" className='home-row-button'>
-                        <img src={require('../../assets/camera.svg')} />
-                        <p>Activities</p>
-                    </Link>
-                </div>
-            </div>
-            <HomeForm date={props.date} handleHomeSubmit={props.handleHomeSubmit} handleDateChange={props.handleDateChange} />
+  return (
+    <div className="home">
+      <div className='home-container'>
+        <div className="home-row">
+          <Link to="#" className='home-row-button home-selected'>
+            <img src={require('../../assets/airplane.svg')} alt='airplane-icon'/>
+            <p>Flights</p>
+          </Link>
+          <Link to="#" className='home-row-button'>
+            <img src={require('../../assets/hotel.svg')} alt='hotel-icon'/>
+            <p>Hotels</p>
+          </Link>
+          <Link to="#" className='home-row-button'>
+              <div>
+                <img src={require('../../assets/airplane.svg')} alt='airplane-icon'/>
+                <img src={require('../../assets/hotel.svg')} alt='hotel-icon'/>
+              </div>
+              <p>Packages</p>
+          </Link>
+          <Link to="#" className='home-row-button'>
+            <img src={require('../../assets/car.svg')} alt='car-icon'/>
+            <p>Cars</p>
+          </Link>
+          <Link to="#" className='home-row-button'>
+            <img src={require('../../assets/cruise.svg')} alt='cruise-icon'/>
+            <p>Cruises</p>
+          </Link>
+          <Link to="#" className='home-row-button'>
+            <img src={require('../../assets/camera.svg')} alt='activities-icon'/>
+            <p>Activities</p>
+          </Link>
         </div>
-    )
+        <HomeForm date={props.date} handleHomeSubmit={props.handleHomeSubmit} handleDateChange={props.handleDateChange} />
+      </div>
+    </div>
+  )
 }
 
 export default Home
