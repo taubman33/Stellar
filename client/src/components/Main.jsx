07c502ecfe66e21bd4625f37215c6b92c9147258
Errol Highberg <errol.highberg@gmail.com>
2020-02-18 09:56:10 -0500

    changed tabs to 2 spaces

--- client/src/components/Main.jsx
+++ client/src/components/Main.jsx
@@ -5,39 +5,43 @@ import TripReview from './routes/TripReview'
 import Book from './routes/Book'
 import Flights from './routes/Flights'
 import Confirmation from './routes/Confirmation'
+import Loading from './Loading'
 import { Route } from 'react-router-dom'
 
 class Main extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = {
+  constructor(props) {
+    super(props)
+    this.state = {
 
-        }
     }
+  }
 
 
-    render() {
-        return (
-            <div className="Main">
-                <Nav />
-                <Route exact path="/">
-                    <Home />
-                </Route>
-                <Route exact path="/flights">
-                    <Flights />
-                </Route>
-                <Route exact path="/trip-review">
-                    <TripReview />
-                </Route>
-                <Route exact path="/booking">
-                    <Book />
-                </Route>
-                <Route exact path="/confirmation">
-                    <Confirmation />
-                </Route>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className="main">
+        <Nav />
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route exact path="/flights">
+          <Flights />
+        </Route>
+        <Route exact path="/trip-review">
+          <TripReview />
+        </Route>
+        <Route exact path="/booking">
+          <Book />
+        </Route>
+        <Route exact path="/confirmation">
+          <Confirmation />
+        </Route>
+        <Route exact path='/loading'>
+          <Loading />
+        </Route>
+      </div>
+    )
+  }
 }
 
 export default Main
