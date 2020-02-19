5ddafaa44ab81ec659a3456e0f456b35446a7704
jeremy taubman <jeremytaubman@jeremys-Air.fios-router.home>
2020-02-19 18:57:47 -0500

    more testing

--- server/tests/routes.test.js
+++ server/tests/routes.test.js
@@ -46,4 +46,36 @@ describe('User API', () => {
     });
   
 
+    //updates a user
+    it('should update an user', async () => {
+      const res = await request(app)
+        .put('/api/users/2')
+        .send({
+          name: 'John Scofield',
+      hashed_password: 'password',
+      number_adults: 6,
+      number_children: 6,
+      departingFlightId: 6,
+      arrivingFlightId: 6
+        });
+      expect(res.statusCode).toEqual(200);
+      expect(res.body).toHaveProperty('user');
+    }),
+
+    
+    //deletes a user
+    it('should delete a user', async () => {
+      const res = await request(app)
+        .del('/api/users/2')
+        .send({
+          name: 'Miles Davis',
+          hashed_password: 'password',
+          number_adults: 2,
+          number_children: 3,
+          departingFlightId: 2,
+          arrivingFlightId: 35
+        });
+      expect(res.statusCode).toEqual(204);
+    });
+
 });
