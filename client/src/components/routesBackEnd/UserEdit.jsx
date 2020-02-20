9a322dc835b4b4007c45a264f550490d42a0a10b
jeremy taubman <jeremytaubman@jeremys-MacBook-Air.local>
2020-02-20 14:28:42 -0500

    reverted to tuesday w itineraries and join table

--- client/src/components/routesBackEnd/UserEdit.jsx
+++ client/src/components/routesBackEnd/UserEdit.jsx
@@ -8,7 +8,7 @@ class UserEdit extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            user: {
+            user: {   
                 title: '',
                 link: ''
             },
